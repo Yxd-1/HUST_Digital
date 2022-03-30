94dd8f3e142c15ee8dcf5399b148c9e558433bf7
youxudong <81860331+Yxd-1@users.noreply.github.com>
2022-03-30 19:39:26 +0800

    Add files via upload

---
+++ decoder/decoder.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:23:20 03/12/2022 
+// Design Name: 
+// Module Name:    decoder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module decoder(
+    input [2:0] in,
+    output [7:0] out
+    );
+	 
+	 reg [7:0] out;
+	 
+	 always @(in)
+	 begin
+		case(in)
+						3'b000: out = 8'b00000001;
+						3'b001: out = 8'b00000010;
+						3'b010: out = 8'b00000100;
+						3'b011: out = 8'b00001000;
+						3'b100: out = 8'b00010000;
+						3'b101: out = 8'b00100000;
+						3'b110: out = 8'b01000000;
+						3'b111: out = 8'b10000000;
+		endcase
+	  end
+endmodule
